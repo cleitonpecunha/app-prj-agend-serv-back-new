a0f58a64ab6369718e8206c8b67133343a254a2a
Cleiton Penido Cunha <cleitonpecunha@gmail.com>
2026-05-11 17:48:29 -0300

    feat: implementar caso de uso para adicionar serviços com validação de usuário

---
+++ src/core/src/service/useCases/serviceAddUseCase.ts
@@ -0,0 +1,39 @@
+import { NotFoundError } from "@/lib/errors";
+import { IServiceAddRequestDTO } from "../dto/serviceDTO";
+import { IServicesRepository } from "../repositories/IServicesRepository";
+import { IUsersRepository } from "../../user/repositories/IUserRepository";
+import Service from "../model/service";
+import { assertProviderOwnership } from "@/lib/auth";
+
+export class ServiceAddUseCase {
+  constructor(
+    private readonly servicesRepository: IServicesRepository,
+    private readonly userRepository: IUsersRepository,
+  ) {}
+
+  async execute(
+    auth: { userId: string },
+    data: IServiceAddRequestDTO,
+  ): Promise<void> {
+    // Verificar se o usuário/prestador existe
+    const [existingUser] = await Promise.all([
+      this.userRepository.findById(data.userId),
+    ]);
+
+    // Se o usuário/prestador não existir, lançar um erro
+    if (!existingUser) {
+      throw new NotFoundError("Usuário/prestador não encontrado.");
+    }
+
+    // Verificar se o usuário autenticado é o proprietário do serviço
+    assertProviderOwnership(auth.userId, existingUser.id!);
+
+    console.log("Dados recebidos para criação do serviço:", data);
+
+    // Criar a instância do serviço e salvar no repositório
+    const service = new Service(data);
+
+    // Salvar o serviço no repositório
+    await this.servicesRepository.save(service);
+  }
+}
