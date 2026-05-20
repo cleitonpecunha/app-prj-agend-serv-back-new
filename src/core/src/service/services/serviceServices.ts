d159bbfe0dcc38a1e92f7c34bf58a1bc57acc9db
Cleiton Penido Cunha <cleitonpecunha@gmail.com>
2026-05-20 17:23:05 -0300

    feat: adicionar método para buscar serviço por ID com validações

--- src/core/src/service/services/serviceServices.ts
+++ src/core/src/service/services/serviceServices.ts
@@ -19,6 +19,21 @@ export class ServiceServices {
     return existService;
   }
 
+  // Método para buscar um servico por ID
+  async buscarServicoPorId(id: string) {
+    const existService = await this.servicesRepository.findById(id);
+
+    if (!existService) {
+      throw new ConflictError(MensagensPadronizadas.SERVICO_NAO_ENCONTRADO);
+    }
+
+    if (!existService.isActive) {
+      throw new ConflictError(MensagensPadronizadas.SERVICO_INATIVO);
+    }
+
+    return existService;
+  }
+
   // Método para validar se um serviço tem agendamento
   async buscarServicosComAgendamento(id: string) {
     const hasAppointments = await this.servicesRepository.hasAppointments(id);
